2059c02e8d42b3495895c9cd81e96533ed3abc50
Caio Moreno <caiomsouza@gmail.com>
2017-06-22 15:01:02 +0200

    Embed Weka Project initial commit
    Embed Weka Project initial commit

---
+++ src/main/java/org/embed_weka/CsvToArff.java
@@ -0,0 +1,40 @@
+package org.embed_weka;
+
+import java.io.File;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+
+public class CsvToArff {
+    public static void main(String[] args) {
+        try {
+                    	
+        	String f1 = "dataset/embed-weka-dataset/kaggle-santander-train.csv";
+            String f2 = "dataset/embed-weka-dataset/kaggle-santander-train.arff";
+            
+
+            System.out.println("Starting ....");
+            
+            // load the CSV file (input file)
+            CSVLoader loader = new CSVLoader();
+            loader.setSource(new File(f1));
+            String [] options = new String[1];
+            options[0]="-H";
+            loader.setOptions(options);
+
+            Instances data = loader.getDataSet();
+            System.out.println(data);
+            
+            System.out.println("Dataset converted to ARFF");
+            
+
+            // save as an  ARFF (output file)
+            ArffSaver saver = new ArffSaver();
+            saver.setInstances(data);
+            saver.setFile(new File(f2));
+            //saver.setDestination(new File(f2));
+            saver.writeBatch();
+        } catch(Exception e) {
+        }
+    }
+}
